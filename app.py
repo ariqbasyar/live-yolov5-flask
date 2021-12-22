eacbe3dc03af79bb9d0f4eb463672a1bf3515a3d
ariqbasyar <ariqbasyar2@gmail.com>
2021-12-22 23:31:32 +0700

    - added delay analytic - added current model selected option - set interval delay to 5s

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ weights = {
 model_key = 'none'
 model = weights[model_key]
 model_changed = False
+models_loaded = False
 
 cnt = 0
 fps = 0
@@ -37,10 +38,22 @@ avg_fps = 0
 min_fps = 999
 max_fps = 0
 start_time = datetime.now()
+delay = 0
+
+def reset_fps():
+    global last_fps, fps, cnt, avg_gps, min_fps, max_fps, start_time, delay
+    cnt = 0
+    fps = 0
+    last_fps = 0
+    avg_fps = 0
+    min_fps = 999
+    max_fps = 0
+    start_time = datetime.now()
+    delay = 0
 
 def gen_frames():  # generate frame by frame from camera
     global fps, cnt, avg_fps, min_fps, max_fps, start_time, last_fps,\
-        model, model_changed
+        model, model_changed, delay
     video = cv2.VideoCapture(0)
     while True:
         # Capture frame-by-frame
@@ -54,6 +67,7 @@ def gen_frames():  # generate frame by frame from camera
         if model_changed:
             model = weights[model_key]
             model_changed = False
+            reset_fps()
         if model is not None:
             pred = detect(model,preprocessed)
             data = box_label(pred, frame)
@@ -65,7 +79,9 @@ def gen_frames():  # generate frame by frame from camera
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + bytes_frame + b'\r\n')
         fps += 1
-        if (datetime.now() - start_time).seconds >= 1:
+        delta = datetime.now() - start_time
+        if delta.seconds >= 1:
+            delay = int(delta.total_seconds() * 1E3)
             last_fps = fps
             avg_fps = (avg_fps * cnt + fps) / (cnt + 1)
             cnt += 1
@@ -85,7 +101,8 @@ def video_feed():
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html', models=weights.keys())
+    global model_key
+    return render_template('index.html', models=weights.keys(), current_model=model_key)
 
 
 @app.route('/analytics')
@@ -95,6 +112,7 @@ def get_analytics():
         'avg': avg_fps,
         'min': min_fps,
         'max': max_fps,
+        'delay': delay,
     }
 
 @app.errorhandler(403)
@@ -112,6 +130,12 @@ def change_model():
     return jsonify(result='OK')
 
 def load_models(device):
+    global models_loaded
+    print(models_loaded)
+    if models_loaded: return
+    models_loaded = True
+    print(f'using {device}')
+    print('Loading models...')
     for key,val in weights.items():
         if val is None: continue
         m = attempt_load(WEIGHTS / val, map_location=device)
@@ -119,4 +143,4 @@ def load_models(device):
 
 if __name__ == '__main__':
     load_models(device)
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', use_reloader=False)
